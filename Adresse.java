6255817304ceaa0f7a9b84b29ff27ac58c834a6c
dhind11 <44953947+dhind11@users.noreply.github.com>
2019-07-06 20:14:51 +0200

    remplissage class adresse comme demandé
    j'attends voir les details dans main et tout , pour voir si j'ai une methode apart l'affichage que je pourrai generer dans cette classe

--- Adresse.java
+++ Adresse.java
@@ -1,3 +1,38 @@
-public class Adresse{ 
-   // met ce que tu doit mettre
+public class Adresse {
+//Attributs
+int numrue,nomrue,codepostal,codeville;
+//Constructors
+public Adresse(int numrue, int nomrue, int codepostal, int codeville) {
+	this.numrue = numrue;
+	this.nomrue = nomrue;
+	this.codepostal = codepostal;
+	this.codeville = codeville;
+}
+public Adresse() {}
+//getters & setters 
+public int getNumrue() {
+	return numrue;
+}
+public void setNumrue(int numrue) {
+	this.numrue = numrue;
+}
+public int getNomrue() {
+	return nomrue;
+}
+public void setNomrue(int nomrue) {
+	this.nomrue = nomrue;
+}
+public int getCodepostal() {
+	return codepostal;
+}
+public void setCodepostal(int codepostal) {
+	this.codepostal = codepostal;
+}
+public int getCodeville() {
+	return codeville;
+}
+public void setCodeville(int codeville) {
+	this.codeville = codeville;
+}
+//Methods
 }
